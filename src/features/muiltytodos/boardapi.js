ebebff54d2b4e5f2bd635cf819653bced8be5868
edupoly <95899099+edupoly@users.noreply.github.com>
2025-01-11 12:52:38 +0530

    Update boardapi.js

--- src/features/muiltytodos/boardapi.js
+++ src/features/muiltytodos/boardapi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const boardapi = createApi({
   reducerPath: 'board',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/Board' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://multitodo-backend-app-production.up.railway.app/Board' }),
   endpoints: (builder) => ({
     gettodolist: builder.query({
       query: () => `/`,
